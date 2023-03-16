62b56d22ac6c89e84250c21f2523e4351b0e5a00
Axel Vanherle <axelvanherle@outlook.com>
2023-03-16 09:56:54 +0100

    Added boilerplate code

--- src/benternet_client/main.cpp
+++ src/benternet_client/main.cpp
@@ -1,8 +1,33 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <string>
+#include <zmq.hpp>
+#include <thread>
 
-int main(int argc, char *argv[])
+#define SLEEP_MS(ms) std::this_thread::sleep_for(std::chrono::milliseconds(ms))
+
+int main(void)
 {
-    QCoreApplication a(argc, argv);
+    std::cout << "test";
 
-    return a.exec();
+    try
+    {
+        zmq::context_t context(1);
+        zmq::socket_t push(context, ZMQ_PUSH);
+
+        push.connect("tcp://benternet.pxl-ea-ict.be:24041");
+
+        while (push.connected())
+        {
+            SLEEP_MS(1000);
+            std::string buffer = "axelvanherle>service>test";
+            push.send(buffer.c_str(), buffer.length());
+            std::cout << "Pushed : " << buffer << std::endl;
+        }
+    }
+    catch (zmq::error_t &ex)
+    {
+        std::cerr << "Caught an exception : " << ex.what();
+    }
+
+    return 0;
 }
--- src/benternet_server/main.cpp
+++ src/benternet_server/main.cpp
@@ -1,8 +1,32 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <string>
+#include <zmq.hpp>
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    QCoreApplication a(argc, argv);
+    try
+    {
+        zmq::context_t context(1);
 
-    return a.exec();
+        // Incoming messages come in here
+        zmq::socket_t subscribe(context, ZMQ_SUB);
+
+        subscribe.connect("tcp://benternet.pxl-ea-ict.be:24042");
+
+        std::string topic = "axelvanherle>service>";
+        subscribe.setsockopt(ZMQ_SUBSCRIBE, topic.c_str(), topic.length());
+
+        zmq::message_t *msg = new zmq::message_t();
+        while (subscribe.connected())
+        {
+            subscribe.recv(msg);
+            std::cout << "Subscribed : [" << std::string((char *)msg->data(), msg->size()) << "]" << std::endl;
+        }
+    }
+    catch (zmq::error_t &ex)
+    {
+        std::cerr << "Caught an exception : " << ex.what();
+    }
+
+    return 0;
 }
